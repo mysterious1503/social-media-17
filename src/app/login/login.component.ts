d9de9745d62cc62b95d498cda0399adbdd45b7d7
Mohita Shrivastava <mohita_shrivastava@epam.com>
2026-02-01 21:46:23 +0530

    Use control flow if and removed common module from app)

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,5 +1,4 @@
 import { Component } from '@angular/core';
-import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
 import { Router, RouterLink } from '@angular/router';
 import { AuthService } from '../services/auth.service';
@@ -7,7 +6,7 @@ import { AuthService } from '../services/auth.service';
 @Component({
   selector: 'app-login',
   standalone: true,
-  imports: [CommonModule, FormsModule, RouterLink],
+  imports: [FormsModule, RouterLink],
   templateUrl: './login.component.html',
   styleUrls: ['./login.component.scss'],
 })
